7a5f1079101f7e3700d6022a0f4d79d0f4a842da
dferfer235 <dferfer235@g.educaand.es>
2021-11-12 14:14:56 +0100

    Bucle for para pedir que se adivine y comprobar si se ha acertado

--- src/main/java/adivinadf/AdivinanzaDF.java
+++ src/main/java/adivinadf/AdivinanzaDF.java
@@ -19,6 +19,8 @@ public class AdivinanzaDF {
 
     public static void main(String[] args) {
         int numero1 = 0, numero2 = 0;
+        boolean ganador = false;
+        int numeroDelUsuario;
         boolean repetirDatos = true;
 
         Scanner teclado = new Scanner(System.in);
@@ -48,7 +50,23 @@ public class AdivinanzaDF {
 
         System.out.println("Escojiendo...");
         int numeroPorAdivinar = adivinar(numero1, numero2);
-        
+
+        for (int vidas = 2; vidas >= 0; vidas--) {
+            System.out.println("¿Que numero crees que es?");
+            numeroDelUsuario = teclado.nextInt();
+
+            if (numeroDelUsuario == numeroPorAdivinar) {
+                System.out.println("LO HAS ADIVINADO");               
+                ganador = true;
+                break;
+            } else {
+                System.out.println("Ese no es");
+                System.out.println("Te quedan " + vidas + " vidas");
+            }
+        }
+        if (ganador==false) {
+            System.out.println("No lo has logrado, el numero era: " + numeroPorAdivinar);
+        }
     }
 
 }
