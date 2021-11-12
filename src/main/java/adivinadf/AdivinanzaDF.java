5bf9cc107005a80a9219814beffd53da75fe1a5f
dferfer235 <dferfer235@g.educaand.es>
2021-11-12 14:41:22 +0100

    Bucle de repetir ejecucion

--- src/main/java/adivinadf/AdivinanzaDF.java
+++ src/main/java/adivinadf/AdivinanzaDF.java
@@ -10,11 +10,10 @@ import java.util.Scanner;
 public class AdivinanzaDF {
 
     private static int adivinar(int n1, int n2) {
-        int numeroAleatorio;
         Random nAleatorio = new Random();
+        int numeroAleatorio;
 
-        numeroAleatorio = nAleatorio.nextInt(n2) + n1;
-        return numeroAleatorio;
+        return numeroAleatorio = nAleatorio.nextInt(n2) + n1;
     }
 
     private static boolean aJugar(int numeroPorAdivinar) {
@@ -49,40 +48,53 @@ public class AdivinanzaDF {
         int numero1 = 0, numero2 = 0;
         boolean ganador;
         boolean repetirDatos = true;
+        boolean repetirPrograma = true;
 
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Comienza la adivinanza");
-        System.out.println("Buscaras un numero aleatorio entre lo que tu decidas");
         do {
-            try {
-                System.out.println("Escoje el numero minimo");
-                numero1 = teclado.nextInt();
+            System.out.println("Comienza la adivinanza");
+            System.out.println("Buscaras un numero aleatorio entre lo que tu decidas");
+            do {
+                try {
+                    System.out.println("Escoje el numero minimo");
+                    numero1 = teclado.nextInt();
 
-                System.out.println("Escoje el numero maximo");
-                numero2 = teclado.nextInt();
+                    System.out.println("Escoje el numero maximo");
+                    numero2 = teclado.nextInt();
 
-                if (numero1 > numero2) {
-                    System.out.println("El numero minimo no puede ser mas pequeño que el maximo");
-                } else {
-                    repetirDatos = false;
+                    if (numero1 > numero2) {
+                        System.out.println("El numero minimo no puede ser mas pequeño que el maximo");
+                    } else {
+                        repetirDatos = false;
+                    }
+
+                } catch (InputMismatchException ime) {
+                    System.out.println("No se ha introducido un numero");
+                    teclado.nextLine();
                 }
 
-            } catch (InputMismatchException ime) {
-                System.out.println("No se ha introducido un numero");
-                teclado.nextLine();
+            } while (repetirDatos);
+
+            System.out.println("Escojiendo...");
+            int numeroPorAdivinar = adivinar(numero1, numero2);
+
+            ganador = aJugar(numeroPorAdivinar);
+
+            if (ganador == false) {
+                System.out.println("No lo has logrado, el numero era: " + numeroPorAdivinar);
             }
+            System.out.println("¿Repetimos?\n"
+            +"1.-Si\n"
+            +"2.-No");
+            int seleccionUsuario = teclado.nextInt();
+            
+            if (seleccionUsuario != 1) {
+                System.out.println("Saliendo...");
+                repetirPrograma = false;
+            }
+        } while (repetirPrograma);
 
-        } while (repetirDatos);
-
-        System.out.println("Escojiendo...");
-        int numeroPorAdivinar = adivinar(numero1, numero2);
-
-        ganador = aJugar(numeroPorAdivinar);
-
-        if (ganador == false) {
-            System.out.println("No lo has logrado, el numero era: " + numeroPorAdivinar);
-        }
     }
 
 }
