1d07461b385f72458f00de5f7b8f95c5f6b34925
dferfer235 <dferfer235@g.educaand.es>
2021-11-12 13:52:18 +0100

    Metodo aleatorio y pregunta de numeros

--- src/main/java/adivinadf/AdivinanzaDF.java
+++ src/main/java/adivinadf/AdivinanzaDF.java
@@ -1,17 +1,35 @@
-
 package adivinadf;
 
-/**
- *
+import java.util.Random;
+import java.util.Scanner;
+
+/*
  * @author davidfdez
  */
 public class AdivinanzaDF {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static int adivinar(int n1, int n2) {
+        int numeroAleatorio;
+        Random nAleatorio = new Random();
+
+        numeroAleatorio = nAleatorio.nextInt(n1) + n2;
+        return numeroAleatorio;
+    }
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        int numero1, numero2;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Comienza la adivinanza");
+        System.out.println("Buscaras un numero aleatorio entre lo que tu decidas");
+
+        System.out.println("Escoje el numero 1");
+        numero1 = teclado.nextInt();
+
+        System.out.println("Escoje el numero 2");
+        numero2 = teclado.nextInt();
+        
     }
 
 }
